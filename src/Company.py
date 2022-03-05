98def5570516959c2c3707e3ee5aecd0d5401ff6
Atakan <buyukoglu.atakan@gmail.com>
2022-03-05 11:17:06 +0100

    Add public company & price fetching

--- src/Company.py
+++ src/Company.py
@@ -1,17 +1,26 @@
 # Defines a company and related classes
-
+from OnlineData import YahooSession
 
 class Company:
 
-    def __init__(self, name, ticker) -> None:
+    def __init__(self, name) -> None:
         # Defining properties of the company
         self.name = name
+
+class PublicCompany(Company):
+
+    def __init__(self, name, ticker, session=None) -> None:
+        super().__init__(name)
         self.ticker = ticker
 
-        if ticker:
-            self.update_variables()
+            
+        # Update online info if a connection is given
+        if session:
+            self.update_online_info(session)
+        else:
+            self.info = None
 
-    
-    def update_variables(self):
-        # TODO: Obtain the parameters online
-        pass
+        
+    def update_online_info(self, session: YahooSession):
+        financials_site = "https://finance.yahoo.com/quote/" + self.ticker + "/financials?p=" + self.ticker
+        self.info = session._parse_json(financials_site)
--- src/Test.py
+++ src/Test.py
@@ -1,12 +1,12 @@
 from OnlineData import YahooSession
+import Company
 
 session = YahooSession(delay=2.0)
 
 ticker = 'MSFT'
-financials_site = "https://finance.yahoo.com/quote/" + ticker + \
-                    "/financials?p=" + ticker
+msft = Company.PublicCompany(ticker, ticker, session)
 
-result = session._parse_json(financials_site)
+result = msft.info
 print(result.keys())
 with open('file.txt', 'w') as file:
     file.write(str(result))
